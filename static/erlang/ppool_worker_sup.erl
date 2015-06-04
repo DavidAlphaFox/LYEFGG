4a0f4435e42e298d5775cd6b9910e99ef2142d0f
David.Gao <david.alpha.fox@gmail.com>
2015-06-04 10:47:56 +0800

    创建镜像

---
+++ static/erlang/ppool_worker_sup.erl
@@ -0,0 +1,13 @@
+-module(ppool_worker_sup).
+-export([start_link/1, init/1]).
+-behaviour(supervisor).
+
+start_link(MFA = {_,_,_}) ->
+    supervisor:start_link(?MODULE, MFA).
+
+init({M,F,A}) ->
+    {ok, {{simple_one_for_one, 5, 3600},
+          [{ppool_worker,
+            {M,F,A},
+            temporary, 5000, worker, [M]}]}}.
+    
